a1f71aff6d45f302298fae06405f8334b975fa61
Oskar Szarmach <szarmach.o@gmail.com>
2023-10-09 13:24:13 +0200

    (Added) Player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,5 +4,39 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    [SerializeField] float playerSwimmingUpVelocity;
+    [SerializeField] float playerRotationSpeed;
+    [SerializeField] float swimmingUpDuration;
+
+    float swimmingUpTimer;
+    bool swimmingUp = false;
+
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (swimmingUp)
+            rb.velocity = Vector3.up * playerSwimmingUpVelocity;
+
+        transform.rotation = Quaternion.Euler(0, 0, rb.velocity.y * playerRotationSpeed);
+    }
+
+    void Update()
+    {
+        swimmingUpTimer -= Time.deltaTime;
+
+        if (swimmingUp && (swimmingUpTimer < 0))
+            swimmingUp = false;
+
+        if (Input.GetKeyDown(KeyCode.Space) && !swimmingUp)
+        {
+            swimmingUp = true;
+            swimmingUpTimer = swimmingUpDuration;
+        }
+    }
 }
